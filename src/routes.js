885a36258f4f017229fa815f88602fcab7f997a2
Thiago Almeida <85900124+thiagoalmeidas@users.noreply.github.com>
2021-07-28 21:00:15 -0300

    commit

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const routes = express.Router();
-const views = __dirname + "/views/pages/"
 
 
-routes.get("/", (req,res) => res.render(views + "index"))
-routes.get("/home", (req,res) => res.render(views + "home"))
+routes.get("/", (req,res) => res.render("pages/index"))
+routes.get("/home", (req,res) => res.render("pages/home"))
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express();
 const routes = require("./routes")
+const path = require("path")
 
+// usando template engine
 app.set("view engine", "ejs");
 
+// Mudar a localizacao da pasta viwes
+app.set('views', path.join(__dirname, 'views'))
+
+// habilitar arquivos statics
 app.use(express.static('public'));
 
 app.use(routes)
+
 
 app.listen(8106)
 console.log("Rodando")
